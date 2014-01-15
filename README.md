82c0e07c5fe22564211a3737651b1c850ecb10dd
tentacode <contact@gabrielpillet.com>
2014-01-15 19:17:10 +0100

    RAD edition setup

---
+++ README.md
@@ -0,0 +1,6 @@
+rad-edition
+===========
+
+The Knp RAD edition of Symfony2
+
+Read the [full documentation](http://rad.knplabs.com/).
---
+++ features/Context/FeatureContext.php
@@ -0,0 +1,8 @@
+<?php
+
+use Behat\Behat\Context\ContextInterface;
+use Behat\Behat\Snippet\Context\TurnipSnippetsFriendlyInterface;
+
+class FeatureContext implements ContextInterface, TurnipSnippetsFriendlyInterface
+{
+}
