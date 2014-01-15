fe2a472764d45edcd93e8bf29953c497e7c58f1f
tentacode <contact@gabrielpillet.com>
2014-01-15 22:16:34 +0100

    Now much search ! But does not work :(

--- src/App/Controller/CatController.php
+++ src/App/Controller/CatController.php
@@ -2,10 +2,21 @@
 
 namespace App\Controller;
 
-class CatController
+class CatController extends Controller
 {
     public function indexAction(array $cats)
     {
         return ['cats' => $cats];
+    }
+
+    public function searchAction()
+    {
+        $term = $this->getRequest()->get('term');
+
+        $cats = [];
+        return [
+            'cats' => $cats,
+            'term' => $term
+        ];
     }
 }
