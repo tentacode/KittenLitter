bd02a254bcbaa1a575fdda1b21f4d251336cb6a3
tentacode <contact@gabrielpillet.com>
2014-01-17 17:03:51 +0100

    Setuping behat and travis

---
+++ features/Context/CatContext.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Context;
+
+use Behat\Behat\Context\Context as ContextInterface;
+use Behat\Behat\Context\TurnipAcceptingContext;
+use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\Mink\Exception\ExpectationException;
+use Behat\Behat\Tester\Exception\PendingException;
+
+class CatContext extends RawMinkContext implements ContextInterface, TurnipAcceptingContext
+{
+    /**
+     * @Given I should see :visibleCatNumber cats out of :totalCatNumber total cats
+     */
+    public function iShouldSeeCatsOutOfTotalCats($visibleCatNumber, $totalCatNumber)
+    {
+        $cats = $this->getListedCats();
+        if (sizeof($cats) != $visibleCatNumber) {
+            throw new ExpectationException(sprintf(
+                '%s cats were expected but only %s were found.',
+                $visibleCatNumber,
+                sizeof($cats)
+            ), $this->getSession());
+        }
+
+        $this->assertTotalCats($totalCatNumber);
+    }
+
+    protected function getListedCats()
+    {
+        return $this
+            ->getSession()
+            ->getPage()
+            ->findAll('css', '.listed-cat')
+        ;
+    }
+
+    protected function assertTotalCats($totalCatNumber)
+    {
+        $this->assertSession()->pageTextContains(sprintf("There's as much as %s cats", $totalCatNumber));
+    }
+}
--- features/Context/FeatureContext.php
+++ features/Context/FeatureContext.php
@@ -1,8 +1,51 @@
 <?php
 
-use Behat\Behat\Context\ContextInterface;
-use Behat\Behat\Snippet\Context\TurnipSnippetsFriendlyInterface;
+namespace Context;
 
-class FeatureContext implements ContextInterface, TurnipSnippetsFriendlyInterface
+use Behat\Behat\Context\Context as ContextInterface;
+use Behat\Behat\Context\TurnipAcceptingContext;
+use Behat\MinkExtension\Context\RawMinkContext;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class FeatureContext extends RawMinkContext implements ContextInterface, TurnipAcceptingContext
 {
+    private $app;
+
+    /**
+     * Initializes context. Every scenario gets its own context object.
+     *
+     * @param array $parameters Suite parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        $this->app = new \AppKernel('test', true);
+        $this->app->boot();
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function purge()
+    {
+        $em = $this->app->getContainer()->get('doctrine.orm.default_entity_manager');
+        $schemaTool = new SchemaTool($em);
+        $schemaTool->dropSchema($em->getMetadataFactory()->getAllMetadata());
+        $schemaTool->createSchema($em->getMetadataFactory()->getAllMetadata());
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function loadAliceData()
+    {
+        exec('app/console doctrine:fixtures:load --env=test --no-interaction');
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function resetSession()
+    {
+        exec('rm -rf app/cache/test/sessions/*');
+    }
 }
