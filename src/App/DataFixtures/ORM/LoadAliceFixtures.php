86fe32a6ec259f54395ce641d67238dfbf651b35
tentacode <contact@gabrielpillet.com>
2014-01-17 11:35:16 +0100

    Creating a custom faker provider for cats and seeding fixtures

--- src/App/DataFixtures/ORM/LoadAliceFixtures.php
+++ src/App/DataFixtures/ORM/LoadAliceFixtures.php
@@ -3,36 +3,21 @@
 namespace App\DataFixtures\ORM;
 
 use Knp\RadBundle\DataFixtures\ORM\LoadAliceFixtures as BaseLoadAliceFixtures;
+use App\Faker\Provider\Cat as CatProvider;
+use Faker\Generator;
 
 class LoadAliceFixtures extends BaseLoadAliceFixtures
 {
-    public function femaleCatName()
+    const SEED = 1337;
+
+    protected function getAliceOptions()
     {
-        $names = ['Sassy','Misty','Missy','Princess','Samatha','Kitty','Puss','Fluffy','Molly','Daisy','Ginger','Midnight','Precious','Maggie','Lucy','Cleo','Whiskers','Chloe','Sophie','Lily','Coco','Boo','Callie','Sadie','Jessie','Jasmine','Nala','Snowball','Angel','Muffin','Pumpkin','Pepper','Baby','Zoe','Peaches','Holly','Dusty','Katie','Sasha','Scooter'];
+        $fakerGenerator = new Generator();
+        $fakerGenerator->seed(self::SEED);
 
-        return $names[array_rand($names)];
-    }
-
-    public function maleCatName()
-    {
-        $names = ['Max','Sam','Tigger','Tiger','Sooty','Smokey','Lucky','Patch','Simba','Smudge','Oreo','Milo','Oscar','Oliver','Buddy','Boots','Harley','Gizmo','Charlie','Toby','Jake','Sebastian','Puffy','Bailey','Buster','Tom','Rocky','Jack','Felix','Spike','Simon','Taz','Rusty','Merlin','Monty','Dusty','Casper','Mittens','Pepper','Blackie'];
-
-        return $names[array_rand($names)];
-    }
-
-    public function catName($gender)
-    {
-        if ($gender === 'Male') {
-            return $this->maleCatName();
-        }
-
-        return $this->femaleCatName();
-    }
-
-    public function gender()
-    {
-        $genders = ['Male', 'Female'];
-
-        return $genders[array_rand($genders)];
+        return array(
+            'providers' => array($this, new CatProvider($fakerGenerator)),
+            'seed' => self::SEED
+        );
     }
 }
